51abcee6240ff348651db56f0ba4f4692952147b
Fritiof Rusck <fritiof@bjornssonrusck.se>
2021-12-18 13:05:21 +0100

    refactor: small stuff

---
+++ helpers/string-printer.js
@@ -0,0 +1,32 @@
+/**
+ * Generates valid brainfuck code that prints a given string
+ * @param {string} text 
+ * @returns Brainfuck code
+ */
+function generatePrintCode(text) {
+  const numbers = [...Buffer.from(text, "ascii")];
+
+  let currentVal = 0;
+  let commands = "";
+
+  for(const num of numbers) {
+    const move = num - currentVal;
+    if(move < 0) {
+      currentVal = currentVal + move;
+      commands += String("-").repeat(Math.abs(move)) + ".";
+    } else {
+      currentVal = currentVal + move;
+      commands += String("+").repeat(move) + ".";
+    }
+  }
+
+  return commands;
+}
+
+const [nodePath, filePath, ...text] = process.argv;
+
+if(text.length === 0) {
+  throw new Error("No text provided");
+} else {
+  console.log(generatePrintCode(text.join(" ")));
+}
